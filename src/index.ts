5e3573cb6a0b6ecdb9e6f3f16d07395830d50cb4
Raul Mendes Rosá <raul.hmetal@gmail.com>
2022-05-03 12:51:02 -0300

    feat: executing conversion via CLI

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,39 @@
 import { CalendarDate } from './@types/Calendar';
 import { makeCalendarManager } from './factories/makeCalendarManager';
+import yargs from 'yargs';
+import { CommandValidation } from './validation/CommandValidation';
 
 const convert = (date: CalendarDate, type: 'gith' | 'theikon') => {
-  const manager = makeCalendarManager(type);
+    const manager = makeCalendarManager(type);
+    const resultManager = makeCalendarManager(
+        type === 'gith' ? 'theikon' : 'gith',
+    );
 
-  return manager.convert(date);
+    const result = manager.convert(date);
+
+    return resultManager.printDate(result);
 };
+
+const [calendar, day, month, year] = yargs.command(
+    '$0 [convert]',
+    'convert date',
+).argv._;
+
+try {
+    CommandValidation.validateSync({ calendar, day, month, year });
+
+    const date = convert(
+        {
+            day: Number(day),
+            month: String(month),
+            year: Number(year),
+        },
+        calendar as 'gith' | 'theikon',
+    );
+
+    console.log(date);
+} catch (error: any) {
+    console.error(
+        `${error.message}. Try \`yarn convert [calendar] [day] [month] [year]`,
+    );
+}
---
+++ src/validation/CommandValidation.ts
@@ -0,0 +1,8 @@
+import * as Yup from 'yup';
+
+export const CommandValidation = Yup.object({
+    calendar: Yup.string().oneOf(['gith', 'theikon']).required(),
+    day: Yup.number().required(),
+    month: Yup.string().required(),
+    year: Yup.number().required(),
+});
